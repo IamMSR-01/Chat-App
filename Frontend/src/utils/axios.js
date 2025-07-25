8c40db2efe6efafa7daef49afdcc99eb157a80cf
Mohd Shaqib Raza <iammsr19413m@gmail.com>
2025-07-25 15:43:00 +0530

    fix: update axios base URL to use environment variable for consistency

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
+// Get the base URL from the environment variables we set on Vercel
+const API_URL = import.meta.env.VITE_BASE_URL;
+
 const API = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5000/api/v1"
-      : "/api",
+  // Append '/api/v1' to the base URL for both development and production
+  baseURL: `${API_URL}/api/v1`,
   withCredentials: true,
 });
 
